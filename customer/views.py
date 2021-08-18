397d9dfcec27c29e5272d48f08be960658c002ca
michel-klein <michel.kps@outlook.com>
2021-08-17 22:02:07 -0300

    Structure and customer page created

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from typing import List
+from django.views.generic import ListView
+from .models import Customer
 
-# Create your views here.
+class CustomerListView(ListView):
+    template_name = "customer/customer_list.html"
+    model = Customer
+    queryset = Customer.objects.all()
